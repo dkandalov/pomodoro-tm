f354a3fa58f1dd4c387e6dd09abf0b138862d3ae
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-02-28 19:54:59 +0000

    made action DumbAware

--- src/pomodoro/PomodoroAction.kt
+++ src/pomodoro/PomodoroAction.kt
@@ -16,9 +16,10 @@ package pomodoro
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.DumbAware
 import pomodoro.model.time.Time
 
-class PomodoroAction : AnAction() {
+class PomodoroAction : AnAction(), DumbAware {
     override fun actionPerformed(event: AnActionEvent) {
         val pomodoroComponent = ApplicationManager.getApplication().getComponent(PomodoroComponent::class.java) ?: return
         pomodoroComponent.model.onUserSwitchToNextState(Time.now())
