69121c3cc733af2b8c87f33626e414e59bd2ebc2
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2022-02-07 22:36:22 +0000

    updated build and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,13 @@
 import org.gradle.api.internal.HasConvention
-import org.jetbrains.kotlin.gradle.dsl.Coroutines
+import org.jetbrains.intellij.IntelliJPluginExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 
-buildscript {
-    repositories {
-        mavenCentral()
-        maven { setUrl("https://oss.sonatype.org/content/repositories/snapshots/") }
-        maven { setUrl("http://dl.bintray.com/jetbrains/intellij-plugin-service") }
-    }
-}
 plugins {
-    java
     idea
-    kotlin("jvm") version "1.3.70"
-    id("org.jetbrains.intellij") version "0.4.18"
+    java
+    kotlin("jvm") version "1.5.30"
+    id("org.jetbrains.intellij") version "1.3.0"
 }
 
 repositories {
@@ -22,19 +15,19 @@ repositories {
 }
 
 dependencies {
-    testCompile("junit:junit:4.12")
+    testImplementation("junit:junit:4.13.2")
 }
 
-fun sourceRoots(block: SourceSetContainer.() -> Unit) = sourceSets.apply(block)
-val SourceSet.kotlin: SourceDirectorySet get() = (this as HasConvention).convention.getPlugin<KotlinSourceSet>().kotlin
+val SourceSet.kotlin: SourceDirectorySet
+    get() = (this as HasConvention).convention.getPlugin<KotlinSourceSet>().kotlin
 
-sourceRoots {
-    getByName("main") {
+sourceSets {
+    main {
         java.srcDirs("./src")
         kotlin.srcDirs("./src")
         resources.srcDirs("./resources")
     }
-    getByName("test") {
+    test {
         kotlin.srcDirs("./test")
     }
 }
@@ -42,27 +35,22 @@ sourceRoots {
 tasks.withType<KotlinJvmCompile> {
     kotlinOptions {
         jvmTarget = "11"
-        apiVersion = "1.3"
-        languageVersion = "1.3"
+        apiVersion = "1.5"
+        languageVersion = "1.5"
     }
 }
 
-kotlin {
-    experimental.coroutines = Coroutines.ENABLE
-}
-
-intellij {
+configure<IntelliJPluginExtension> {
     // See https://www.jetbrains.com/intellij-repository/releases
-    val ideVersion = System.getenv().getOrDefault("IJ_VERSION",
-        "201.6668.113"
-//        "LATEST-EAP-SNAPSHOT"
+    version.set(
+        System.getenv().getOrDefault(
+            "IJ_VERSION",
+//        "201.6668.113"
+            "LATEST-EAP-SNAPSHOT"
+        )
     )
-    println("Using ide version: $ideVersion")
-    version = ideVersion
-    pluginName = "pomodoro"
-    downloadSources = true
-    sameSinceUntilBuild = false
-    updateSinceUntilBuild = false
+    pluginName.set("pomodoro")
+    downloadSources.set(true)
+    sameSinceUntilBuild.set(false)
+    updateSinceUntilBuild.set(false)
 }
-
-val Any.kotlin: SourceDirectorySet get() = withConvention(KotlinSourceSet::class) { kotlin }
