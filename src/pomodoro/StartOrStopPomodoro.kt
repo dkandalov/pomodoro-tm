c0241fb97acbb160f156dc34130cd8c4e716d9ce
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2022-02-13 17:39:04 +0000

    added ResetPomodorosCounter action

--- src/pomodoro/StartOrStopPomodoro.kt
+++ src/pomodoro/StartOrStopPomodoro.kt
@@ -6,8 +6,14 @@ import com.intellij.openapi.components.service
 import com.intellij.openapi.project.DumbAware
 import pomodoro.model.time.Time
 
-class PomodoroAction : AnAction(), DumbAware {
+class StartOrStopPomodoro : AnAction(), DumbAware {
     override fun actionPerformed(event: AnActionEvent) {
         service<PomodoroService>().model.onUserSwitchToNextState(Time.now())
+    }
+}
+
+class ResetPomodorosCounter : AnAction(), DumbAware {
+    override fun actionPerformed(event: AnActionEvent) {
+        service<PomodoroService>().model.resetPomodoros()
     }
 }
